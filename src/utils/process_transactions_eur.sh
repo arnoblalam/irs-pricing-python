034cdca9829e2ad64422822769c134b108555157
Arnob Alam <arnoblalam@gmail.com>
2024-02-23 12:55:37 -0500

    Add script to process EUR transactions

---
+++ src/utils/process_transactions_eur.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+
+# Set the base directories for transactions, yield curves, and output files
+transaction_base="data/processed/EUR"
+yield_curve_base="data/raw/curves/EUR"
+output_base="data/results/EUR"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_base"
+
+# Function to convert date format from YYYY-MM-DD to MMDDYYYY
+function convert_date_format() {
+    echo "$(echo $1 | cut -c6-7)$(echo $1 | cut -c9-10)$(echo $1 | cut -c1-4)"
+}
+
+# Function to convert date format from YYYY-MM-DD to MMDDYYYY
+function convert_date_format_2() {
+    echo "$(echo $1 | cut -c1-4)$(echo $1 | cut -c6-7)$(echo $1 | cut -c9-10)"
+}
+
+# Loop through all transaction files in the transaction_base directory
+for transaction_file in "$transaction_base"/*_output.xlsx; do
+    # Extract the date from the transaction file name
+    date_str=$(basename "$transaction_file" "_output.xlsx")
+
+    # Convert date_str to the format used in the curve data files
+    # curve_date_str=$(convert_date_format "$date_str")
+    curve_date_str=$(convert_date_format_2 "$date_str")
+
+    # Set the corresponding yield curve file path
+    yield_curve_file="$yield_curve_base/EUR_Curve_${curve_date_str}.xlsx"
+
+    # Set the output file path
+    output_file="$output_base/EUR_${date_str}_priced.xlsx"
+
+    # Set the evaluation date
+    evaluation_date="$date_str"
+
+    # Run the price_swaps.py script with the required arguments
+    ./src/price_swaps.py "$transaction_file" "$yield_curve_file" "$output_file" "$evaluation_date"
+done
