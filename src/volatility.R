83e39085afa5ab64cdc704359960bb79db0150fe
Arnob Alam <arnoblalam@gmail.com>
2024-04-14 17:57:47 -0400

    Update volatility.R

--- src/volatility.R
+++ src/volatility.R
@@ -4,37 +4,14 @@ library(dplyr)
 library(lubridate)
 library(broom)
 
-# Load the data
-data <- read_excel('data/volatility/combined_trade_data.xlsx')
 
-# Convert 'Effective', 'Maturity', and 'Trade Time' columns to datetime format
-data$Effective <- ymd(data$Effective)
-data$Maturity <- ymd(data$Maturity)
-data$`Trade Time` <- ymd_hms(data$`Trade Time`)
-
-# Add some additional columns
-data$Trade_Date <- as.Date(data$`Trade Time`)
-
-# Log price
-data$log_rate_1 <- ifelse(data$`Rate 1` > 0, log(data$`Rate 1`), NA)
-
+# Helper functions
 # Categorize trade time
 categorize_trade_time <- function(time) {
-  if (hour(time) >= 8 & hour(time) < 11) {
-    return('morning')
-  } else if (hour(time) >= 11 & hour(time) < 14) {
-    return('mid day')
-  } else if (hour(time) >= 14 & hour(time) < 17) {
-    return('afternoon')
-  } else {
-    return('off hours')
-  }
+  ifelse((hour(time) >= 8 & hour(time) < 11), 'morning',
+    ifelse(hour(time) >= 11 & hour(time) < 14, 'mid day',
+           ifelse(hour(time) >= 14 & hour(time) < 17, 'afternoon', 'off hours')))
 }
-
-data$trade_time_categorical <- sapply(data$`Trade Time`, categorize_trade_time)
-
-data$Grp <- ifelse(data$Curr == 'USD', 1,
-                   ifelse(data$Curr == 'CAD', 0, -1))
 
 # Define a function to check if the trade date falls within the specified periods
 in_period <- function(trade_date) {
@@ -48,20 +25,41 @@ in_period <- function(trade_date) {
   return(as.integer(within_any_period))
 }
 
+# Load the data
+data <- read_excel('data/volatility/combined_trade_data.xlsx')
+
+# Convert 'Effective', 'Maturity', and 'Trade Time' columns to datetime format
+data$Effective <- ymd(data$Effective)
+data$Maturity <- ymd(data$Maturity)
+data$`Trade Time` <- ymd_hms(data$`Trade Time`)
+
+# Add some additional columns
+data$Trade_Date <- as.Date(data$`Trade Time`)
+
+# Convert rates specified in bps to percentages
+data <- data %>% 
+  mutate(`Rate 1` = ifelse(`Rate 1` > 10, `Rate 1`/100, `Rate 1`))
+
+data <- data %>% 
+  mutate(trade_time_categorical = categorize_trade_time(`Trade Time`))
+
+data$Grp <- ifelse(data$Curr == 'USD', 1,
+                   ifelse(data$Curr == 'CAD', 0, -1))
+
 # Filter and process the data
 data_filtered <- data %>%
   filter(Type == 'IRS Fix-Float',
          CD == 'TR',
          `Leg 2` %in% c('USD-LIBOR-BBA', 'CAD-BA-CDOR'),
          Curr %in% c('USD', 'CAD'),
-         wday(`Trade Time`) < 6,
+         wday(`Trade Time`, week_start = 1) < 6,
          date(`Trade Time`) != mdy('09022013'),
          date(`Trade Time`) != mdy('05272013'),
-         `T` %in% c('1Y', '2Y', '3Y', '4Y', '5Y', '6Y', '7Y', '8Y', '9Y', '10Y', '15Y', '30Y')) %>%
+         `T` %in% c('1Y', '2Y', '3Y', '4Y', '5Y', '6Y', '7Y',
+                    '8Y', '9Y', '10Y', '15Y', '30Y')) %>%
   arrange(Curr, `T`, Trade_Date, `Trade Time`) %>%
   group_by(Curr, `T`, Trade_Date) %>%
-  mutate(return_1 = c(NA, diff(log_rate_1))) %>%
-  ungroup()
+  mutate(return_1 = `Rate 1`/lag(`Rate 1`))
 
 # Group by and summarize
 new_data <- data_filtered %>%
