43dd2fb0da211db2f6541b178b1df0bf0f661dd2
Arnob Alam <arnoblalam@gmail.com>
2024-06-23 11:32:21 -0400

    Create roll_2.sh

---
+++ src/utils/roll_2.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+src/liquidity/roll_measure.py data/liquidity/USD_20130211-20130222.xlsx data/liquidity/Roll/USD_Phase1_pretrend_10.xlsx 10 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130513-20130524.xlsx data/liquidity/Roll/USD_Phase2_pretrend_10.xlsx 10 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130812-20130823.xlsx data/liquidity/Roll/USD_Phase3_pretrend_10.xlsx 10 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130812-20130823.xlsx data/liquidity/Roll/USD_Phase3_pretrend_2.xlsx 2 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130513-20130524.xlsx data/liquidity/Roll/USD_Phase2_pretrend_2.xlsx 2 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130211-20130222.xlsx data/liquidity/Roll/USD_Phase1_pretrend_2.xlsx 2 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130211-20130222.xlsx data/liquidity/Roll/USD_Phase1_pretrend_5.xlsx 5 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130513-20130524.xlsx data/liquidity/Roll/USD_Phase2_pretrend_5.xlsx 5 USD
+src/liquidity/roll_measure.py data/liquidity/USD_20130812-20130823.xlsx data/liquidity/Roll/USD_Phase3_pretrend_5.xlsx 5 USD
+
+src/liquidity/roll_measure.py data/liquidity/CAD_20130211-20130222.xlsx data/liquidity/Roll/CAD_Phase1_pretrend_10.xlsx 10 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130513-20130524.xlsx data/liquidity/Roll/CAD_Phase2_pretrend_10.xlsx 10 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130812-20130823.xlsx data/liquidity/Roll/CAD_Phase3_pretrend_10.xlsx 10 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130812-20130823.xlsx data/liquidity/Roll/CAD_Phase3_pretrend_2.xlsx 2 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130513-20130524.xlsx data/liquidity/Roll/CAD_Phase2_pretrend_2.xlsx 2 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130211-20130222.xlsx data/liquidity/Roll/CAD_Phase1_pretrend_2.xlsx 2 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130211-20130222.xlsx data/liquidity/Roll/CAD_Phase1_pretrend_5.xlsx 5 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130513-20130524.xlsx data/liquidity/Roll/CAD_Phase2_pretrend_5.xlsx 5 CAD
+src/liquidity/roll_measure.py data/liquidity/CAD_20130812-20130823.xlsx data/liquidity/Roll/CAD_Phase3_pretrend_5.xlsx 5 CAD
