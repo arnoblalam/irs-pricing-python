cf988cea2be6a1643842210f444e7534d042a627
Arnob Alam <arnoblalam@gmail.com>
2024-10-05 20:25:45 -0400

    Liquidity measures

--- src/liquidity/Roll_with_stock.R
+++ src/liquidity/Roll_with_stock.R
@@ -1,8 +1,9 @@
 library(readxl)
 library(lubridate)
 library(tidyverse)
+library(stargazer)
 
-data <- read_excel("data/Roll_combined_2_with_stock.xlsx")
+data <- read_excel("data/liquidity/Roll_combined_2_with_stock.xlsx")
 
 filtered_data <- 
   data %>%
@@ -10,3 +11,4 @@ filtered_data <-
 
 model <- lm(`Roll Measure` ~ Group*pd + as.factor(Tenor) + stock + volatility, 
             data = filtered_data)
+stargazer(model, type = "html", out = "reports/Roll_measure.html")
